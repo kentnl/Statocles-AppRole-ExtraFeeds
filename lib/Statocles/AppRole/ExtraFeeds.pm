ceed0214b4ebe983406176f1d681429da83e9763
Kent Fredric <kentfredric@gmail.com>
2017-03-07 22:19:29 +1300

    Build results of b81292b (on master)

--- lib/Statocles/AppRole/ExtraFeeds.pm
+++ lib/Statocles/AppRole/ExtraFeeds.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Statocles::AppRole::ExtraFeeds;
 
-our $VERSION = '0.001003';
+our $VERSION = '0.001004';
 
 # ABSTRACT: Generate additional feed sets for apps
 
@@ -113,7 +113,7 @@ Statocles::AppRole::ExtraFeeds - Generate additional feed sets for apps
 
 =head1 VERSION
 
-version 0.001003
+version 0.001004
 
 =head1 EXPERIMENTAL
 
